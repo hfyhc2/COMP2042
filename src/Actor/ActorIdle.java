a1bd012574414ecb1eeb825a9710e58a734b5285
hfyhc2 <76203199+hfyhc2@users.noreply.github.com>
2020-12-19 03:25:38 +0800

    Update ActorIdle.java

--- src/Actor/ActorIdle.java
+++ src/Actor/ActorIdle.java
@@ -1,16 +1,21 @@
-package Actor;
+ackage Actor;
+
+/**
+ * this functionality is for objects that have no runtime 
+ *
+ */
 
 public class ActorIdle implements ActorInterface {
 
 	@Override
 	public void actor(Actor actor, long now) {
-		// does nothing
+		
 
 	}
 
 	@Override
 	public void restrictmovement(Actor actor) {
-		// nothing
+		
 
 	}
 
