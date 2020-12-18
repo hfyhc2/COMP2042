3bdc9ec3232c21a31c222cd3fb5ee7b5fa9ed77a
hfyhc2 <76203199+hfyhc2@users.noreply.github.com>
2020-12-19 03:34:12 +0800

    Update ViewGame.java

--- src/p4_group_8_repo/ViewGame.java
+++ src/p4_group_8_repo/ViewGame.java
@@ -7,8 +7,6 @@ import javafx.scene.Scene;
 import java.util.*;
 import Highscore.ScoreHandler;
 import World.MyStage;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.CornerRadii;
@@ -22,12 +20,17 @@ import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
-
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+/**
+ * handles all scene related work 
+ *
+ */
 public class ViewGame {
 	public Scene menuscene, infoscene;
 
-	int xres = 500;
-	int yres = 800;
+	int xres = 600;
+	int yres = 600;
 
 	String username;
 
@@ -39,19 +42,12 @@ public class ViewGame {
 	public Frog frog;
 	GameManager GM;
 	public ScoreHandler SH;
-
 	Scene back;
-
-	// private TableView table;
-
 	AnimationTimer timer;
 
 	/**
-	 * The constructor instantiates all nessecary objects for the scenes and sets
-	 * the menu scene to display
-	 * 
-	 * @param PrimaryStage This is the base scene that is used to change whats
-	 *                     displayed
+	 * this constructor instantiates all objects for the scenes 
+	 * @param PrimaryStage  is the base scene that is used to change what is displayed
 	 */
 	public ViewGame(Stage PrimaryStage) {
 		this.primaryStage = PrimaryStage;
@@ -70,87 +66,85 @@ public class ViewGame {
 	}
 
 	/**
-	 * This function is called to create the menu scene
+	 * create the menu scene
 	 * 
-	 * @return A reference to the menu scene that can be used to set the primary
-	 *         stage to
 	 */
 	private Scene CreatMenu() {
 
 		Label label1 = new Label("Welcome to FROGGER");
 		label1.setFont(new Font("Arial", 32));
-
+		
+	
 		Button button1 = new Button("PLAY!");
 		button1.setOnAction(e -> primaryStage.setScene(creategamescene()));
 		button1.setMinSize(150, 70);
+		button1.setStyle("-fx-text-fill: #00ff00;-fx-background-color:black");
+		
 
-		button1.setStyle("-fx-text-fill: #00ff00;-fx-background-color:Green");
-		// button1.setStyle("-fx-background-color:Green");
-
-		Button button2 = new Button("Exit :(");
+		Button button2 = new Button("Exit ");
 		button2.setOnAction(e -> primaryStage.close());
 		button2.setMinSize(150, 70);
-		button2.setStyle("-fx-text-fill: #00ff00;-fx-background-color:Green");
+		button2.setStyle("-fx-text-fill: #00ff00;-fx-background-color:black");
 
-		Button button3 = new Button("info");
+		Button button3 = new Button("Info");
 		button3.setOnAction(e -> primaryStage.setScene(infoscene));
 		button3.setMinSize(150, 70);
-		button3.setStyle("-fx-text-fill: #00ff00;-fx-background-color:Green");
+		button3.setStyle("-fx-text-fill: #00ff00;-fx-background-color:black");
 
 		Button button4 = new Button("Scoreboard");
 		button4.setOnAction(e -> primaryStage.setScene(createtable()));
 		button4.setMinSize(150, 70);
-		button4.setStyle("-fx-text-fill: #00ff00;-fx-background-color:Green");
-
+		button4.setStyle("-fx-text-fill: #00ff00;-fx-background-color:black");
+		
+		
 		VBox layout1 = new VBox();
 		layout1.setSpacing(50);
 		layout1.setAlignment(Pos.CENTER);// Changed the alignment to center-left
 		layout1.getChildren().addAll(label1, button1, button3, button4, button2);
-		layout1.setBackground(
-				new Background(new BackgroundFill(Color.rgb(0, 255, 0), CornerRadii.EMPTY, Insets.EMPTY)));
+		layout1.setBackground(new Background(new BackgroundFill(Color.rgb(255, 255, 255), CornerRadii.EMPTY, Insets.EMPTY)));
 		Scene menuscene = new Scene(layout1, xres, yres);
 		back = menuscene;
 		return menuscene;
 	}
 
 	/**
-	 * This method is used to create an info screen scene
+	 *  create an info screen scene
 	 * 
-	 * @return a reference to the infor screen scene to set the primary stage to
 	 */
 	private Scene CreateInfo() {
 		Button button4 = new Button("Back");
 		button4.setOnAction(e -> primaryStage.setScene(menuscene));
 		button4.setMinSize(100, 50);
-		button4.setStyle("-fx-text-fill: #00ff00;-fx-background-color:Green");
+		button4.setStyle("-fx-text-fill: #00ff00;-fx-background-color:black");
 
 		Label label1 = new Label("Get to the end!!!!!!!");
 		label1.setFont(new Font("Arial", 32));
 
 		Label label2 = new Label("Move with WASD and");
 		label2.setFont(new Font("Arial", 32));
+		
+		Image background=new Image("file:src/resources/arcade.png", 600, 300, true, false);
+		ImageView iv1 = new ImageView();
+        iv1.setImage(background);
+
 
 		VBox layout2 = new VBox();
-		layout2.getChildren().addAll(label2, label1, button4);
+		layout2.getChildren().addAll(iv1,label2, label1, button4);
 		layout2.setSpacing(50);
-		layout2.setBackground(
-				new Background(new BackgroundFill(Color.rgb(0, 255, 0), CornerRadii.EMPTY, Insets.EMPTY)));
-		layout2.setAlignment(Pos.CENTER);// Changed the alignment to center-left
+		layout2.setBackground(new Background(new BackgroundFill(Color.rgb(0, 255, 0), CornerRadii.EMPTY, Insets.EMPTY)));
+		layout2.setAlignment(Pos.CENTER);
 		return (new Scene(layout2, xres, yres));
 	}
 
 	/**
-	 * This method creates the game scene and starts everything required for the
-	 * game to run
+	 *  creates the game scene and starts everything needed for the game to run
 	 * 
-	 * @return A referene to the game scene that the primary stage can be set to
-	 * @see {@link InitObjects#InitObjects()}
-	 *      {@link sessionHandler#sessionHandler(ViewManager)}
 	 */
 	Scene creategamescene() {
-		background = new MyStage(); // stage is aplication window
-		Scene scene = new Scene(background, xres, yres);// change x to 500, maybe not here
-		// creates new scene with background as root node
+		int xres = 500;
+		int yres = 800;
+		background = new MyStage(); 
+		Scene scene = new Scene(background, xres, yres);
 		background.start();
 		objects.addobjects(background);
 		GM = new GameManager(this);
@@ -159,10 +153,8 @@ public class ViewGame {
 	}
 
 	/**
-	 * This method creates a scene with a table that shows the highest 10 scores of
-	 * previous players
+	 *  creates a table that shows the highest 10 scores 
 	 * 
-	 * @return a reference to the table scene that the primary stage can be set to
 	 */
 	public Scene createtable() {
 		TableView table = new TableView();
@@ -195,7 +187,7 @@ public class ViewGame {
 		Button button4 = new Button("Back");
 		button4.setOnAction(e -> primaryStage.setScene(back));
 		button4.setMinSize(100, 50);
-		button4.setStyle("-fx-text-fill: #00ff00;-fx-background-color:Green");
+		button4.setStyle("-fx-text-fill: #00ff00;-fx-background-color:black");
 
 		VBox layout2 = new VBox();
 		layout2.getChildren().addAll(table, button4);
@@ -208,10 +200,9 @@ public class ViewGame {
 	}
 
 	/**
-	 * This method creates and sets a win screen for the player that apears when the
-	 * player completes 10 levels
+	 *  creates the win screen for the player 
 	 */
-	public void createwin() {
+	public void createwinscreen() {
 
 		Label label1 = new Label("YOU WIN!!.");
 		label1.setFont(new Font("Arial", 32));
@@ -222,19 +213,19 @@ public class ViewGame {
 		Button button4 = new Button("Scoreboard");
 		button4.setOnAction(e -> primaryStage.setScene(createtable()));
 		button4.setMinSize(150, 70);
-		button4.setStyle("-fx-text-fill: #00ff00;-fx-background-color:Green");
+		button4.setStyle("-fx-text-fill: #00ff00;-fx-background-color:black");
 
 		Button button2 = new Button("Exit");
 		button2.setOnAction(e -> primaryStage.close());
 		button2.setMinSize(150, 70);
-		button2.setStyle("-fx-text-fill: #00ff00;-fx-background-color:Green");
+		button2.setStyle("-fx-text-fill: #00ff00;-fx-background-color:black");
 
 		TextField textField = new TextField();
 		textField.setMaxSize(150, 70);
 
 		Button button3 = new Button("Submit username");
 		button3.setMinSize(150, 70);
-		button3.setStyle("-fx-text-fill: #00ff00;-fx-background-color:Green");
+		button3.setStyle("-fx-text-fill: #00ff00;-fx-background-color:black");
 
 		button3.setOnAction(action -> {
 			SH.writescores(textField.getText(), frog.getPoints(), background.lvl);
@@ -244,8 +235,7 @@ public class ViewGame {
 		layout1.setSpacing(50);
 		layout1.setAlignment(Pos.CENTER);// Changed the alignment to center-left
 		layout1.getChildren().addAll(label1, label2, textField, button3, button4, button2);
-		layout1.setBackground(
-				new Background(new BackgroundFill(Color.rgb(0, 255, 0), CornerRadii.EMPTY, Insets.EMPTY)));
+		layout1.setBackground(new Background(new BackgroundFill(Color.rgb(255, 255, 255), CornerRadii.EMPTY, Insets.EMPTY)));
 		Scene winscene = new Scene(layout1, xres, yres);
 		back = winscene;
 		primaryStage.setScene(winscene);
