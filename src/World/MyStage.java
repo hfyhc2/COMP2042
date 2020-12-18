2590f6ec2d24dccbd69b746b3b46f770cbc31a50
hfyhc2 <76203199+hfyhc2@users.noreply.github.com>
2020-12-19 03:35:05 +0800

    Update MyStage.java

--- src/World/MyStage.java
+++ src/World/MyStage.java
@@ -7,9 +7,7 @@ import javafx.scene.media.MediaPlayer;
 import javafx.util.Duration;
 
 /**
- * This class
- * 
- * @author khaled
+ * this class plays the music
  *
  */
 public class MyStage extends World {
@@ -19,7 +17,10 @@ public class MyStage extends World {
 	public MyStage() {
 
 	}
-
+	/**
+	 * plays the music
+	 *
+	 */
 	public void playMusic() {
 		String musicFile = "src/resources/Frogger Main Song Theme (loop).mp3";
 		Media sound = new Media(new File(musicFile).toURI().toString());
@@ -27,7 +28,10 @@ public class MyStage extends World {
 		mediaPlayer.setCycleCount(MediaPlayer.INDEFINITE);
 		mediaPlayer.play();
 	}
-
+	/**
+	 * stops the music
+	 *
+	 */
 	public void stopMusic() {
 		mediaPlayer.stop();
 	}
