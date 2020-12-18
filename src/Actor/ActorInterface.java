68147fc9b1f165d54296a2c1048866cfa27c75f4
hfyhc2 <76203199+hfyhc2@users.noreply.github.com>
2020-12-19 03:25:57 +0800

    Update ActorInterface.java

--- src/Actor/ActorInterface.java
+++ src/Actor/ActorInterface.java
@@ -1,21 +1,22 @@
 package Actor;
 
+/**
+ * abstract class for functionality
+ *
+ */
+
 public interface ActorInterface {
 
 	/**
-	 * abstract method for act functionality that represents how each object should
-	 * act in game time
-	 * 
-	 * @param actor the object that needs functionality
-	 * @param now   game timer
+	 * abstract method for actor functionality when in game time
+	 * @param actor is the object that needs functionality and now is for game time
 	 */
-
+	
 	public void actor(Actor actor, long now);
 
 	/**
-	 * This method restricts the actor from going off screen
-	 * 
-	 * @param actor the object that needs restriction
+	 * This method stops actor from going off screen
+	 * @param actor is the object that is restricted
 	 */
 	public void restrictmovement(Actor actor);
 }
