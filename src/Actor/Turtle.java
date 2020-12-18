4612e140d456965ca31b917ce9af5bb518912431
hfyhc2 <76203199+hfyhc2@users.noreply.github.com>
2020-12-19 03:27:06 +0800

    Update Turtle.java

--- src/Actor/Turtle.java
+++ src/Actor/Turtle.java
@@ -1,50 +1,56 @@
 package Actor;
 
+
 import javafx.scene.image.Image;
 
 /**
- * This functionality is used for turtles and wet turtles
- * 
- * @author khaled
+ * this functionality is for turtles and wet turtles class
  *
  */
 public class Turtle implements ActorInterface {
 
-	/**
-	 * aside from movement, this method is also in charge of movement animations
-	 */
+
 	public void actor(Actor actor, long now) {
-
+		
+	
 		restrictmovement(actor);
-
-		if (now / 900000000 % 4 == 0) {
+		
+		if (now/900000000  % 4 ==0) {
 			actor.setImage(actor.image2);
 			actor.sunk = false;
-
-		} else if (now / 900000000 % 4 == 1) {
+			
+		}
+		else if (now/900000000 % 4 == 1) {
 			actor.setImage(actor.image1);
 			actor.sunk = false;
-		} else if (now / 900000000 % 4 == 2) {
+		}
+		else if (now/900000000 %4 == 2) {
 			actor.setImage(actor.image3);
 			actor.sunk = false;
-		} else if (now / 900000000 % 4 == 3) {
-			if (actor.image4 != null) {
-				actor.setImage(actor.image4);
-				actor.sunk = true;
+		} else if (now/900000000 %4 == 3) {
+			if (actor.image4!=null) {
+			actor.setImage(actor.image4);
+			actor.sunk = true;
 			}
 		}
+	
+		actor.move(actor.speed , 0);
 
-		actor.move(actor.speed, 0);
+
 
 	}
 
+	/**
+	 * stops turtles from going off screen
+	 */
 	@Override
-	public void restrictmovement(Actor actor) {// if sprite goes out of frame, put it back at beginning
+	public void restrictmovement(Actor actor) {
 
-		if (actor.getX() > 600 && actor.speed > 0)
+		if (actor.getX() > 600 && actor.speed>0)
 			actor.setX(-200);
-		if (actor.getX() < -75 && actor.speed < 0)
+		if (actor.getX() < -100 && actor.speed<0)
 			actor.setX(600);
-
+		
 	}
+
 }
