a498269861456b15887dbe03800b57706e084272
hfyhc2 <76203199+hfyhc2@users.noreply.github.com>
2020-12-19 03:24:53 +0800

    Update Actor.java

--- src/Actor/Actor.java
+++ src/Actor/Actor.java
@@ -5,53 +5,60 @@ import javafx.scene.image.ImageView;
 import java.util.ArrayList;
 import Actor.Actor;
 import World.World;
-
+/**
+ * This abstract class is used to define all objects in the game
+ *
+ */
 public abstract class Actor extends ImageView {
-
+	/**
+	 * Type stores the type of object to be added, is used for the slot system in class world
+	 */
 	protected String type;
+	/**
+	 * The functionality of each object
+	 */
 	protected ActorInterface actor;
+	/**
+	 * the speed for moving objects
+	 */
 	protected double speed;
-
+	/**
+	 * movement animations and death animations
+	 */
 	protected Image image1;
 	protected Image image2;
 	protected Image image3;
 	protected Image image4;
-
+	/**
+	 * Frog's variables
+	 */
 	protected boolean sunk = false;
 	public static int points;
 	protected boolean carDeath, waterDeath;
 	protected double movement;
 
 	protected int imgSize;
-
 	/**
-	 * This method is used to move the object a number of pixels in the x direction
-	 * and a number of pixels in the y direction
-	 * 
-	 * @param dx how many pixels to move in the x direction
-	 * @param dy how many pixels to move in the y direction
+	 * this method moves the object in the x and y axis 
+	 * @param dx amount of distance to be moved on the x axis
+	 * @param dy amount of distance to moved on the y axis
 	 */
 	public void move(double dx, double dy) {
 		setX(getX() + dx);
 		setY(getY() + dy);
 	}
 
-	/**
-	 * fetches the parent object, which is the stage
-	 * 
-	 * @return the parent node, the root node
-	 */
+	
 	public World getWorld() {
 		return (World) getParent();
 	}
 
 	/**
-	 * Method to check intersection between objects
-	 * 
-	 * @param <A> an empty arraylist
-	 * @param cls The object to check intersections with
-	 * @return an array containing all actors that are intersecting
-	 */
+     * this method checks the intersection between objects
+     * @param <A> type of class to check for an interaction
+	 * @param cls object of class to check for interaction
+	 * @return returns an array containing objects intersected with
+     */
 	public <A extends Actor> java.util.List<A> getIntersectingObjects(java.lang.Class<A> cls) {
 		ArrayList<A> someArray = new ArrayList<A>();
 		for (A actor : getWorld().getObjects(cls)) {
@@ -61,32 +68,17 @@ public abstract class Actor extends ImageView {
 		}
 		return someArray;
 	}
-
-	/**
-	 * types of actors such as turtle or log, used for placement functions
-	 * 
-	 * @return the type of actor
-	 * @see World#checknextfree(String)
-	 */
+	
+	
 	public String getType() {
 		return type;
 	}
 
-	/**
-	 * A call to the encapsulated method act
-	 * 
-	 * @param now game timer
-	 */
 	public void actor(long now) {
 		actor.actor(this, now);
 
 	}
 
-	/**
-	 * method for animal
-	 * 
-	 * @param now
-	 */
 	public void handledeath(long now) {
 
 	}
