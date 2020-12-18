d554401cd6a260d4f191fcab8ec6e73e3168b4e5
hfyhc2 <76203199+hfyhc2@users.noreply.github.com>
2020-12-19 03:28:13 +0800

    Update ATurtle.java

--- src/Objects/ATurtle.java
+++ src/Objects/ATurtle.java
@@ -2,37 +2,39 @@ package Objects;
 
 import Actor.*;
 import javafx.scene.image.Image;
-
+/**
+ * this class moves the turtles 
+ *
+ */
 public class ATurtle extends Actor {
 
 	int dim = 110;
 
 	/**
-	 * the constructor assigns images of the trutle and a type to be used to
-	 * determine position
-	 * 
-	 * @param xpos position of object on the x axis
-	 * @param s    the speed of the object
+	 * assigns the turtle to a position
+	  * @param imageLink the sprite of the turtle
+	 * @param s speed of turtle
+	 * @param w width of turtle image
+	 * @param h height of turtle image
 	 */
 	public ATurtle(int xpos, int s, int w, int h) {
 		int temp = 0;
 		int ypos = 0;
 
 		actor = new Turtle();
-
-		// import turtle sprites
-
 		image1 = new Image("file:src/resources/TurtleAnimation1.png", dim, dim, true, true);
 		image2 = new Image("file:src/resources/TurtleAnimation2.png", dim, dim, true, true);
 		image3 = new Image("file:src/resources/TurtleAnimation3.png", dim, dim, true, true);
 		image4 = null;
-		type = "Turtle" + String.valueOf(s);
+		type="TurtleLeft+String.valueOf(s)";
 
 		if (s > 0) {
 
 			image1 = new Image("file:src/resources/TurtleAnimation1mirror.png", dim, dim, true, true);
 			image2 = new Image("file:src/resources/TurtleAnimation2mirror.png", dim, dim, true, true);
 			image3 = new Image("file:src/resources/TurtleAnimation3mirror.png", dim, dim, true, true);
+			image4 = null;
+			type="TurtleRight+String.valueOf(s)";
 
 		}
 
@@ -44,7 +46,11 @@ public class ATurtle extends Actor {
 		speed = s;
 		setImage(image2);
 	}
-
+	
+	/**
+	 * allow the player to move with the turtle
+	 * @return the speed of the turtle
+	 */
 	public double getspeed() {
 		return speed;
 	}
