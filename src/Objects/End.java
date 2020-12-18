d1d14e901587223b63b9ffc58c645d53218237d9
hfyhc2 <76203199+hfyhc2@users.noreply.github.com>
2020-12-19 03:29:13 +0800

    Update End.java

--- src/Objects/End.java
+++ src/Objects/End.java
@@ -2,19 +2,21 @@ package Objects;
 
 import Actor.*;
 import javafx.scene.image.Image;
-
+/**
+ * This class is used for the goal
+ *
+ */
 public class End extends Actor {
 	boolean activated = false;
 	int dim = 60;
 	int ypos = 95;
 	int xpos[] = { 8, 135, 265, 395 };
 	static int i = 0;
-
+	
 	/**
-	 * The constructor initiaites the functionality and has an array that stores the
-	 * positions that each hole should be in
+	 * Sets the goal has an array to store for each goal
 	 */
-	public End() { // initialize end hole
+	public End() { 
 
 		actor = new ActorIdle();
 
@@ -28,19 +30,18 @@ public class End extends Actor {
 	}
 
 	/**
-	 * used to trigger the hole once the player has reached it
+	 * shows that the player reached the goal
 	 */
-	public void setEnd() { // add frog image to indicate it triggered
+	public void setEnd() { 
 		setImage(new Image("file:src/resources/FrogEnd.png", dim, dim, true, true));
 		activated = true;
 	}
 
 	/**
-	 * used to check if the hole has been activated
-	 * 
-	 * @return a boolean indicating if the hole has been activated or not
+	 * used to check if the goal has been activated
+	 * @return is a boolean indicating if the hole has been activated or not
 	 */
-	public boolean isActivated() { // tells other classes if hole is activated or not
+	public boolean isActivated() {
 		return activated;
 	}
 
