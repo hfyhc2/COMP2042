4f9bdcde6cdc0bb5d15891ac9fceca537324f41f
hfyhc2 <76203199+hfyhc2@users.noreply.github.com>
2020-12-19 03:28:56 +0800

    Update Digit.java

--- src/Objects/Digit.java
+++ src/Objects/Digit.java
@@ -2,18 +2,15 @@ package Objects;
 
 import Actor.*;
 import javafx.scene.image.Image;
-
 /**
- * this class is for the digits used for score and has no runtime functionality
- * 
- * @author khaled
+ * this class is for the digits used for score 
  *
  */
-public class Digit extends Actor {
-	int dim; // dimensions
-	Image im1;
 
-	// gets p4_group_8_repo for numbers and puts it in place
+public class Digit extends Actor {
+	int dim; 
+	Image im1;
+	
 	public Digit(int n, int dim, int x, int y) {
 
 		actor = new ActorIdle();
