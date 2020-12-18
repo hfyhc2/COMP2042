71420fea61a1a89216743c502c2987d6b475cfc3
hfyhc2 <76203199+hfyhc2@users.noreply.github.com>
2020-12-19 03:34:49 +0800

    Update coordinate.java

--- src/World/coordinate.java
+++ src/World/coordinate.java
@@ -1,18 +1,21 @@
 package World;
 
+/**
+ * This class defines the coordinate positions for objects 
+ *
+ */
 public class coordinate {
 
 	public int x;
 	public int y;
-
+	
 	/**
-	 * Constructor for class
-	 * 
-	 * @param x x coordinate
-	 * @param y Y coordinate
+	 * the constructor
+	 * @param x coordinate
+	 * @param Y coordinate
 	 */
-	public coordinate(int x, int y) {
-		this.x = x;
-		this.y = y;
+	public coordinate(int x,int y) {
+		this.x=x;
+		this.y=y;
 	}
 }
